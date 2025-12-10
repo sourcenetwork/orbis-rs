40d69159c8f16c7e587b96cd17b725145a57680e
Jesse Abramowitz <jesse@entropy.xyz>
2025-12-10 12:18:57 -0500

    orbis server

--- bin/orbis-node/src/main.rs
+++ bin/orbis-node/src/main.rs
@@ -1,13 +1,41 @@
 // Include the generated proto code
+pub mod server;
+
+use clap::Parser;
+use server::CryptoServiceImpl;
+use std::net::SocketAddr;
+use tonic::{Request, Response, Status};
 pub mod crypto_service {
     tonic::include_proto!("crypto_service");
 }
+use crypto_service::{
+    crypto_service_server::{CryptoService, CryptoServiceServer},
+    EncryptionRequest, EncryptionResponse, StartDkgRequest, StartDkgResponse,
+};
 
-use crypto_service::crypto_service_client::CryptoServiceClient;
+#[derive(Parser, Debug)]
+#[command(name = "orbis-node")]
+#[command(about = "Orbis CryptoService gRPC server")]
+struct Args {
+    /// Address to bind the server to
+    #[arg(short, long, default_value = "[::1]:50051")]
+    addr: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Connect to the server
-    let _client = CryptoServiceClient::connect("http://[::1]:50051").await?;
+    let args = Args::parse();
+    let addr: SocketAddr = args.addr.parse()?;
+
+    println!("Starting CryptoService gRPC server on {}", addr);
+    println!("Server is ready to accept connections...");
+
+    let service = CryptoServiceImpl::default();
+
+    tonic::transport::Server::builder()
+        .add_service(CryptoServiceServer::new(service))
+        .serve(addr)
+        .await?;
+
     Ok(())
 }
