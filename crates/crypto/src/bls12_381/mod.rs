e2af7ad27a6e79f688c648a433fa893e0f3fee3e
Jesse Abramowitz <jesse@entropy.xyz>
2025-12-15 10:46:08 -0500

    PRE trait

--- crates/crypto/src/bls12_381/mod.rs
+++ crates/crypto/src/bls12_381/mod.rs
@@ -1,2 +1,3 @@
 pub mod common;
 pub mod dkg;
+pub mod pre;
--- crates/crypto/src/trait.rs
+++ crates/crypto/src/trait.rs
@@ -37,7 +37,7 @@ pub trait PolynomialCommitment: Clone + Debug + Send + Sync {
     /// Verify a share against this commitment using constant-time comparison
     fn verify_share(&self, share_id: u32, share_value: &Self::ShareValue) -> bool;
 }
-
+/// Trait for DKG
 pub trait Dkg: Send + Sync {
     type ShareValue;
     type PublicKey;
@@ -92,4 +92,92 @@ pub trait Dkg: Send + Sync {
     ///
     /// This is used for verification in the re-encryption protocol
     fn compute_public_polynomial(&self) -> Result<Self::PubPoly>;
+}
+
+/// Trait for PRE
+pub trait ThresholdDealer {
+    type DistKeyShare;
+    type Secret;
+    type PublicKey;
+    type ShareValue;
+    type ReencryptReply;
+    type PubPoly: PubPoly<PublicKey = Self::PublicKey>;
+    type PubShare;
+
+    /// Re-encrypt a secret share using the receiver's public key.
+    ///
+    /// Input:
+    ///   dkg_ski (ski) - Private share of secret key of DKG.
+    ///   rdr_pk  (xG)  - Public key of the reader.
+    ///   enc_cmt (rG)  - Schnorr commit of encoded keys.
+    ///
+    /// Output:
+    ///   xnc_ski (Ui) - Re-encrypted secret share.
+    ///   chlgi  (ei)  - Random oracle challenge.
+    ///   proofi (fi)  - NIZK proof of re-encryption.
+    fn reencrypt(
+        &self,
+        dist_key_share: &Self::DistKeyShare,
+        scrt: &Self::Secret,
+        rdr_pk: &Self::PublicKey,
+    ) -> Result<Self::ReencryptReply>;
+
+    /// Verify a re-encryption proof.
+    ///
+    /// Input:
+    ///   rdr_pk  (xG)  - Public key of the reader.
+    ///   enc_cmt (rG)  - Schnorr commit of encoded keys.
+    ///   dkg_ski (Ui)  - Re-encrypted share of commitment.
+    ///   chlgi  (ei)   - Random oracle challenge at index i.
+    ///   proofi (fi)   - NIZK proof of re-encryption at index i.
+    ///   dkg_cmt (ci)  - Commitment (public polynomial) of DKG at index i.
+    fn verify(
+        &self,
+        rdr_pk: &Self::PublicKey,
+        dkg_cmt: &Self::PubPoly,
+        enc_cmt: &Self::PublicKey,
+        reply: &Self::ReencryptReply,
+    ) -> Result<()>;
+
+    /// Recover the re-encrypted commitment from shares
+    fn recover(
+        &self,
+        xnc_ski: &[Self::PubShare],
+        t: usize,
+        n: usize,
+    ) -> Result<Option<Self::PublicKey>>;
+
+    // TODO: next two functions are not needed in PRE at the node but for encryptor and decryptor
+    // may want to remove them from this trait, however it does need to exist for an implementation to be complete and tested
+    // think on this
+
+    /// Encrypt a secret using the aggregate public key of the DKG.
+    ///
+    /// Input:
+    ///   dkg_pk (sG) - Aggregate public key of the DKG.
+    ///   scrt  (k)   - Secret to be encrypted.
+    ///
+    /// Output:
+    ///   enc_cmt  - Schnorr commit (rG)
+    ///   enc_scrt - Encrypted key-slices (rsG + Ki)
+    fn encrypt_secret(
+        dkg_pk: &Self::PublicKey,
+        data: &[u8],
+    ) -> Result<(Self::PublicKey, Self::Secret)>;
+
+    /// Decrypt a secret using the reader's secret key.
+    ///
+    /// Input:
+    ///   dkg_pk  (sG)       - Aggregate public key of DKG.
+    ///   xnc_cmt (rsG + xsG) - Re-encrypted schnorr-commit.
+    ///   rdr_sk  (x)        - Secret key of the reader.
+    ///
+    /// Output:
+    ///   scrt - Recovered secret.
+    fn decrypt_secret(
+        dkg_pk: &Self::PublicKey,
+        xnc_cmt: &Self::PublicKey, // Recovered from re-encryption
+        rdr_sk: &Self::ShareValue,
+        secret: &Self::Secret,
+    ) -> Result<Vec<u8>>;
 }
